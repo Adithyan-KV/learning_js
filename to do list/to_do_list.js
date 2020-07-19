59d270be2a4e2361574bbf4bedb87daa9744556b
Adithyan KV <adithyankv007@gmail.com>
2020-07-20 02:11:01 +0530

    "Task-completed" and "Delete task" functionality added
    -A delete button was added which deletes its corresponding task
    -A checkbox was added that strikes off text when checked
    -Still have to package them all into one web component that gets added
     when the user types in a task and submits.

--- to do list/to_do_list.js
+++ to do list/to_do_list.js
@@ -1,9 +1,19 @@
 document.addEventListener('DOMContentLoaded',()=>{
+    //adding elements
     document.querySelector('#task-input').onsubmit=addElement;
-    checkbox=document.querySelector("input[name='task-completed']");
-    checkbox.onclick = strikeOff;
+
+    //marking tasks as completed
+    let checkbox=document.querySelectorAll("input[name='task-completed']");
+    checkbox.forEach(element => {
+                element.addEventListener('click',strikeOff)
+            });
+
+    //deleting an element
+    let delete_buttons= document.querySelectorAll("button[name='delete-task']");
+    delete_buttons.forEach(element => element.addEventListener('click',deleteTask))
 });
 
+//function that adds a new task to the todo list
 addElement=()=>{
     let input = document.querySelector("#input-entry").value;
     //to prevent empty inputs making it into the list
@@ -19,7 +29,21 @@ addElement=()=>{
     }
 };
 
+//strikes off the text of the label corresponding to the checkbox
 //not using arrow function as i need to use "this"
 function strikeOff(){
-    alert(this.parentNode);
+    let label=this.parentNode.querySelector("label[for='task']");
+    //toggles the strikethrough depending on state of checkbox
+    if (this.checked){
+        label.style.textDecoration='line-through';
+    }
+    else{
+        label.style.textDecoration='none';
+    }
+};
+
+//deletes the task
+function deleteTask(){
+    //remove the list item corresponding to the delte button that was pressed
+    this.parentNode.remove();
 };
