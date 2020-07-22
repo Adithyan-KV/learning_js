a90a5c16f14cb998fa1a282478b568771f059b61
Adithyan KV <adithyankv007@gmail.com>
2020-07-22 18:03:24 +0530

    Added shadow DOM and templates, Broke counter
    -A shadow dom was added to encapsulate the styling.
    -The styling was changed to a more minimalist style. Personally quite
     like it
    -The markup was moved from inside the JS to an HTML template inside
     the HTML file
    -Actually broke all the logic. Counter doesn't work now.
    TODO
    -Re-add all the event listeners and implement the logic so that the
     counter works again.

--- counter 2/CounterComponent.js
+++ counter 2/CounterComponent.js
@@ -6,49 +6,55 @@ class CounterComponent extends HTMLElement{
     constructor(){
         super();
 
+        //adding shadow DOM
+        this.attachShadow({mode:'open'});
+
+        //adding template HTML markup to create component
+        const template = document.querySelector('#counter-component');
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+
         //state of counter
         this.count = 0;
     };
 
     connectedCallback(){
+        console.log("element added to DOM");
+    }
 
-        //adding the HTML content of the counter
-        var content_markup=`<div class='container'>
-                                <h2>Counter</h2>
-                                <div class=count>${this.count}</div>
-                                <div class='button-container'>
-                                    <button type='button' name='increment' id='increment'>+</button>
-                                    <button type='button' name='decrement' id='decrement'>-</button>
-                                    <br>
-                                    <button type='button' name='reset' id='reset'>Reset</button>
-                                </div>
-                            </div>`
+    disconnectedCallback(){
+        console.log("element removed from DOM");
+    }
 
-
-        this.innerHTML=content_markup;
-
-        //incrementing
-        let incr_btn=this.querySelector("#increment");
-        incr_btn.addEventListener('click',()=>{
-            this.count++;
-            let count =  this.querySelector('.count');
-            count.innerHTML = this.count;
-        });
-
-        //decrementing
-        let decr_btn=this.querySelector('#decrement');
-        decr_btn.addEventListener('click',()=>{
-            this.count--;
-            let count = this.querySelector('.count');
-            count.innerHTML = this.count;
-        });
-
-        let reset_btn=this.querySelector('#reset');
-        reset_btn.addEventListener('click',()=>{
-            this.count=0;
-            this.querySelector('.count').innerHTML = this.count;
-        });
-    };
+    // connectedCallback(){
+    //
+    //     //adding the HTML content of the counter
+    //     var content_markup=``
+    //
+    //
+    //     this.innerHTML=content_markup;
+    //
+    //     //incrementing
+    //     let incr_btn=this.querySelector("#increment");
+    //     incr_btn.addEventListener('click',()=>{
+    //         this.count++;
+    //         let count =  this.querySelector('.count');
+    //         count.innerHTML = this.count;
+    //     });
+    //
+    //     //decrementing
+    //     let decr_btn=this.querySelector('#decrement');
+    //     decr_btn.addEventListener('click',()=>{
+    //         this.count--;
+    //         let count = this.querySelector('.count');
+    //         count.innerHTML = this.count;
+    //     });
+    //
+    //     let reset_btn=this.querySelector('#reset');
+    //     reset_btn.addEventListener('click',()=>{
+    //         this.count=0;
+    //         this.querySelector('.count').innerHTML = this.count;
+    //     });
+    // };
 };
 
 window.customElements.define('counter-component',CounterComponent);
