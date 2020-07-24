5190f47be82d7a002639049d66764a92e0e905c7
Adithyan KV <adithyankv007@gmail.com>
2020-07-25 01:25:32 +0530

    Logic for task item implemented
    -Eventlisteners were added to the list items in the todo list.
    -Now the list items can be marked as done and also deleted
    
    TODO
    -Have to implement a pin to top functionality

--- To do list 2/TodoComponent.js
+++ To do list 2/TodoComponent.js
@@ -28,6 +28,31 @@ document.addEventListener('DOMContentLoaded',()=>{
             let template=document.querySelector('.task-component');
             this.shadowRoot.appendChild(template.content.cloneNode(true));
         };
+
+        connectedCallback(){
+            let shadow=this.shadowRoot;
+
+            //toggling strikethrough of text depending on state of checkbox
+            let checkbox=this.shadowRoot.querySelector("input[name='task-completed']");
+            checkbox.addEventListener('click',toggleStrikeThrough);
+
+            function toggleStrikeThrough(){
+                let label=shadow.querySelector('#task-text');
+                if(checkbox.checked){
+                    label.style.setProperty('text-decoration','line-through');
+                    label.style.setProperty('color','var(--disabled-text)');
+                }
+                else{
+                    label.style.setProperty('text-decoration','none');
+                    label.style.setProperty('color','var(--lighter-hints)');
+                }
+            };
+
+            //removing task if the delete button is clicked
+            let close_btn=this.shadowRoot.querySelector("button[name='close']");
+            close_btn.addEventListener('click',()=>this.remove());
+
+        }
     };
 
     customElements.define('task-component',TaskComponent);
